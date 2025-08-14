dca2fbd9a2f4b5357a6ce9c2d0e2ca96389eef5e
YugalRajVimal <sagaryugal472@gmail.com>
2025-08-14 16:00:52 +0530

    --

--- Controllers/CustomerControllers/Customer.auth.controller.js
+++ Controllers/CustomerControllers/Customer.auth.controller.js
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { broadcast } from "../../server.js";
 
+
 class CustomerAuthController {
   sendNotification(name, comment) {
     broadcast({ name, comment });
@@ -260,24 +261,30 @@ class CustomerAuthController {
       });
       const hour = new Date(now).getHours();
 
+      // 01:00 AM - 08:00 AM → 1.5 to 2 minutes
       if (hour >= 1 && hour < 8)
         return Math.floor(Math.random() * 30000) + 90000; // 90s - 120s
+
+      // 08:00 AM - 10:00 AM → 8 to 10 seconds
       if (hour >= 8 && hour < 10)
         return Math.floor(Math.random() * 2000) + 8000; // 8s - 10s
+
+      // 10:00 AM - 09:00 PM → 2 to 4 seconds
       if (hour >= 10 && hour < 21)
         return Math.floor(Math.random() * 2000) + 2000; // 2s - 4s
+
+      // 09:00 PM - 01:00 AM → 15 to 20 seconds
       return Math.floor(Math.random() * 5000) + 15000; // 15s - 20s
     };
 
     const sendNext = async () => {
-      const delay = getDelayByTime(); // Get delay first
-      console.log("Scheduling next comment after delay:", delay);
+      console.log("Sending next comment in sequence");
+      const delay = getDelayByTime(); // Pick delay according to IST time
+      await this.sendSingleNameComment(); // Send message
+      
 
-      setTimeout(async () => {
-        console.log("Sending next comment in sequence");
-        await this.sendSingleNameComment(); // Send message
-        sendNext(); // Schedule again
-      }, delay);
+      console.log("Scheduling next comment after delay:", delay);
+      setTimeout(sendNext, delay); // Schedule next
     };
 
     sendNext();
