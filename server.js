d9e7adf348e3fd0aa72797e6c5b81caaee4a9eb1
YugalRajVimal <sagaryugal472@gmail.com>
2025-09-18 21:09:19 +0530

    --

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
+      "http://localhost:3001",
       "https://theprmedia.com",
       "https://www.theprmedia.com",
     ],
